48b89e9985f0489d4a06966328c57a67245df4e8
Jarwovick <jarwovick@gmail.com>
2026-09-17 12:29:47 +0700

    Initialize DatabaseProvider in ToolskuApp

--- app/src/main/java/com/toolsku/app/ToolskuApp.kt
+++ app/src/main/java/com/toolsku/app/ToolskuApp.kt
@@ -2,6 +2,7 @@ package com.toolsku.app
 
 import android.app.Application
 import com.toolsku.app.core.Prefs
+import com.toolsku.app.core.db.DatabaseProvider
 
 class ToolskuApp : Application() {
 
@@ -9,6 +10,7 @@ class ToolskuApp : Application() {
         super.onCreate()
         try {
             Prefs.init(this)
+            DatabaseProvider.init(this)
         } catch (e: Exception) {
             e.printStackTrace()
         }
