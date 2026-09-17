693788640219a32288873d234890994083f46e94
Jarwovick <jarwovick@gmail.com>
2026-09-17 17:44:20 +0700

    Update EmptyAction.kt

--- app/src/main/java/com/toolsku/app/killer/action/EmptyAction.kt
+++ app/src/main/java/com/toolsku/app/killer/action/EmptyAction.kt
@@ -3,10 +3,6 @@ package com.toolsku.app.killer.action
 import com.toolsku.app.killer.core.ActionStep
 import com.toolsku.app.killer.core.StepResult
 
-/**
- * Action placeholder — langsung sukses.
- * Tiru dari `xy1` Baxa.
- */
 class EmptyAction(
     parentStep: ActionStep
 ) : BaseAction(parentStep) {
